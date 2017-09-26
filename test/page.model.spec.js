03a5d3974419cb6442db148f14178e7a1e9a3cbf
erika <samuels.erika@yahoo.com>
2017-09-26 15:46:19 -0400

    completed virtual specs

---
+++ test/page.model.spec.js
@@ -0,0 +1,53 @@
+const Page = require ('../models').Page;
+const chai = require('chai');
+const expect = chai.expect;
+const spies = require('chai-spies');
+chai.use(spies);
+
+
+describe('Page model', function () {
+
+      describe('Virtuals', function () {
+        var page;
+        beforeEach(function(){
+            page = Page.build();       //creates a virtual table
+        });
+        describe('route', function () {
+          it('returns the url_name prepended by "/wiki/"', function () {
+            page.urlTitle = 'WTF_this_is_a_title';
+            expect (page.route).to.equal('/wiki/WTF_this_is_a_title');
+          });
+        });
+        describe('renderedContent', function () {
+          it('converts the markdown-formatted content into HTML', function () {
+              page.content = 'this is some sample text';
+              expect(page.renderedContent).to.equal('<p>this is some sample text</p>\n');
+          });
+        });
+      })
+    
+      describe('Class methods', function () {
+        describe('findByTag', function () {
+          it('gets pages with the search tag');
+          it('does not get pages without the search tag');
+        });
+      });
+    
+      describe('Instance methods', function () {
+        describe('findSimilar', function () {
+          it('never gets itself');
+          it('gets other pages with any common tags');
+          it('does not get other pages without any common tags');
+        });
+      });
+    
+      describe('Validations', function () {
+        it('errors without title');
+        it('errors without content');
+        it('errors given an invalid status');
+      });
+    
+      describe('Hooks', function () {
+        it('it sets urlTitle based on title before validating');
+      });
+    });
